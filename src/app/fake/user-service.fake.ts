fe6cb6cefcad2c880c6e56906f1c66a5bd1c4a3c
Joseph Woolley <joe@calledtoconstruct.net>
2021-10-23 19:37:13 -0400

    Cleaning up fake user service.

--- src/app/fake/user-service.fake.ts
+++ src/app/fake/user-service.fake.ts
@@ -2,18 +2,23 @@ import { User } from '@angular/fire/auth';
 import { ReplaySubject } from 'rxjs';
 
 export class FakeUserService {
-  public signInWithGoogleCalled = 0;
-  public signInWithGitHubCalled = 0;
-  public signOutCalled = 0;
+
   public readonly user$ = new ReplaySubject<User | null>(1);
   public readonly loggedIn$ = new ReplaySubject<boolean>(1);
   public readonly notLoggedIn$ = new ReplaySubject<boolean>(1);
+
+  public signInWithGoogleCalled = 0;
+  public signInWithGitHubCalled = 0;
+  public signOutCalled = 0;
+
   public signInWithGoogle(): void {
     this.signInWithGoogleCalled++;
   }
+  
   public signInWithGitHub(): void {
     this.signInWithGitHubCalled++;
   }
+
   public signOut(): void {
     this.signOutCalled++;
   }
@@ -23,11 +28,13 @@ export class FakeUserService {
     this.loggedIn$.next(true);
     this.notLoggedIn$.next(false);
   }
+
   public setUpNotLoggedIn(): void {
     this.user$.next(null);
     this.loggedIn$.next(false);
     this.notLoggedIn$.next(true);
   }
+
   public tearDown(): void {
     this.user$.complete();
     this.loggedIn$.complete();

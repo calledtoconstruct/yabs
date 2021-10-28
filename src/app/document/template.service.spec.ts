e85689e4191f000d396e01438553bc317fc4588d
Joseph Woolley <joe@calledtoconstruct.net>
2021-10-27 22:47:16 -0400

    Extracting placeholders from template text in template service.

--- src/app/document/template.service.spec.ts
+++ src/app/document/template.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TemplateService } from './template.service';
+import { Placeholder, TemplateService } from './template.service';
 
 describe('TemplateService', () => {
   let service: TemplateService;
@@ -13,4 +13,66 @@ describe('TemplateService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('extract placeholders for', () => {
+
+    describe('when given a valid template', () => {
+
+      const template = `
+      This is some text for the test template named \${name}.  It is expected to have a number of
+      \${placeholders: string}.  Some of which may be optional, as in \${this-one: number, optional}.
+      \${valida-tion}
+      \${valida_tion}
+      \${Validation}
+      \${validation}
+      \${validation: string}
+      \${validation: number}
+      \${validation: phone-number}
+      \${validation: currency}
+      \${validation: date}
+      \${validation: time}
+      \${validation: date-time}
+      \${pattern-to-match, break}
+      \${pattern-to-match, keep}
+      \${pattern-to-match, table[abc,asf]}
+      \${pattern-to-match: string, break}
+      \${pattern-to-match: string, keep}
+      \${pattern-to-match: string, table[abc,asf]}
+      \${pattern-to-match: string, break, table[asdf]}
+      \${pattern-to-match: string, keep, table[abc,asf]}.
+      \${pattern-to-match: string, break, keep}
+      \${pattern-to-match: string, break, keep, table[asdf]}
+      `;
+      let placeholders: Array<Placeholder>;
+
+      beforeEach(() => {
+        placeholders = service.extractPlaceholdersFrom(template);
+      });
+
+      it('should return an array of placeholders', () => {
+        expect(placeholders).toBeTruthy();
+      });
+
+      it('should return twenty four placeholders', () => {
+        expect(placeholders.length).toBe(24);
+      });
+
+      it('should return placeholder for pattern-to-match with break option', () => {
+        expect(placeholders[14].name).toBe('pattern-to-match');
+        expect(placeholders[14].dataType).toBe('string');
+        expect(placeholders[14].break).toBe(true);
+        expect(placeholders[14].keep).toBe(false);
+      });
+
+      it('should return placeholder for pattern-to-match of type string with break and keep options', () => {
+        expect(placeholders[22].name).toBe('pattern-to-match');
+        expect(placeholders[22].dataType).toBe('string');
+        expect(placeholders[22].break).toBe(true);
+        expect(placeholders[22].keep).toBe(true);
+      });
+
+    });
+
+  });
+
 });
--- src/app/document/template.service.ts
+++ src/app/document/template.service.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+const dataTypeExpressions = [
+  { name: 'string', expression: '(?:string)' },
+  { name: 'number', expression: '(?:number)' },
+  { name: 'phoneNumber', expression: '(?:phone-number)' },
+  { name: 'currency', expression: '(?:currency)' },
+  { name: 'date', expression: '(?:date)' },
+  { name: 'time', expression: '(?:time)' },
+  { name: 'dateTime', expression: '(?:date-time)' },
+  { name: 'address', expression: '(?:address)' }
+]
+  .map(dataType => dataType.expression)
+  .join('|');
+
+const placeholderNameExpression = `([a-zA-Z\\-\\_]*)`;
+const dataTypeExpression = `(?:\\:\\s?(${dataTypeExpressions}))?`;
+const optionalExpression = '(?:\\,\\s?(optional))?';
+const breakExpression = '(?:\\,\\s?(break))?';
+const keepExpression = '(?:\\,\\s?(keep))?';
+const tableExpression = '(?:\\,\\s?(table\\[.*?\\]))?';
+const extractPlaceholderDefinitionExpression = `\\$\\{${placeholderNameExpression}${dataTypeExpression}${optionalExpression}${breakExpression}${keepExpression}${tableExpression}\\}`;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,9 +33,57 @@ export class TemplateService {
       text: 'vuianuiwesdafaaasdf'
     });
   }
+
+  public extractPlaceholdersFrom(templateText: string): Array<Placeholder> {
+    const regex = RegExp(extractPlaceholderDefinitionExpression, 'mg');
+
+    const placeholders = new Array<Placeholder>();
+
+    let result: RegExpExecArray | null;
+    while (!!(result = regex.exec(templateText))) {
+      const match = result[0];
+      const name = result[1];
+      const dataType = result[2];
+      const optional = result[3];
+      const hardBreak = result[4];
+      const keep = result[5];
+      const table = result[6];
+      placeholders.push(<Placeholder>{
+        name: name,
+        dataType: !!dataType ? dataType : 'string',
+        optional: optional === 'optional',
+        break: hardBreak === 'break',
+        keep: keep === 'keep',
+        tableDefinition: null
+      });
+    };
+
+    return placeholders;
+  }
+
 }
 
 export interface Template {
   title: string;
   text: string;
+}
+
+export type PlaceholderDataType = 'string' | 'number' | 'phone-number' | 'currency' | 'date' | 'time' | 'date-time';
+
+export interface SimplePlaceholder {
+  name: string;
+  dataType: PlaceholderDataType;
+  optional: boolean;
+}
+
+export interface Placeholder extends SimplePlaceholder {
+  break: boolean;
+  keep: boolean;
+  tableDefinition: TableDefinition | null;
+}
+
+export interface TableDefinition {
+  columns: Array<SimplePlaceholder>;
+  minimumRows: number;
+  maximumRows: number;
 }

c8023a1a19354fd8bc1e1d9d57dffedb073df171
Joseph Woolley <joe@calledtoconstruct.net>
2021-11-01 20:17:00 -0400

    Verifying that correct number of placeholders is returned from extract placeholders for on template service.

--- src/app/document/form-page/form-page.component.spec.ts
+++ src/app/document/form-page/form-page.component.spec.ts
@@ -4,17 +4,25 @@ import { Placeholder, Template, TemplateService } from '../template.service';
 import { ActivatedRoute } from '@angular/router';
 import { FakeActivatedRoute } from 'src/app/fake/activated-route.fake';
 import { FormPageComponent } from './form-page.component';
+import { CountContainer } from 'src/app/test/count-container.type';
 
 describe('Document -> Form Page', () => {
 
+  const firstPlaceholder = '${first-placeholder: string}';
+  const secondPlaceholder = '${second-placeholder: number, break}';
+
   const template = <Template>{
-    text: '${first-placeholder: string}'
+    text: `${firstPlaceholder} and ${secondPlaceholder}`
   };
 
   const placeholders = [
     <Placeholder>{
       name: 'first-placeholder',
       dataType: 'string'
+    }, <Placeholder>{
+      name: 'second-placeholder',
+      dataType: 'number',
+      break: true
     }
   ];
 
@@ -73,7 +81,7 @@ describe('Document -> Form Page', () => {
 
     });
 
-  FakeActivatedRoute.whenRouteIsActivated(
+  const getResult = FakeActivatedRoute.whenRouteIsActivated(
     { 'templateIdentifier': 'asdfasdf' },
     { 'templateIdentifier': 1 },
     placeholders,
@@ -82,6 +90,22 @@ describe('Document -> Form Page', () => {
 
       it('should call extract placeholders from on template service', () => {
         expect(templateService.extractPlaceholdersFrom).toHaveBeenCalled();
+      });
+
+      describe('result', () => {
+
+        let result: Array<Placeholder>;
+        let hasWasCalledFor: CountContainer;
+        let getWasCalledFor: CountContainer;
+
+        beforeEach(() => {
+          [result, hasWasCalledFor, getWasCalledFor] = getResult();
+        });
+
+        it('should have the correct count of placeholders', () => {
+          expect(result.length).toBe(2);
+        });
+        
       });
 
     });

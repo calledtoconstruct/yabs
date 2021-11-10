147a15aa23594640c04bddd1ad9f8a8a3bcd8557
Joseph Woolley <joe@calledtoconstruct.net>
2021-11-10 08:09:23 -0500

    Adding header to read article comment component.

--- src/app/read/comment/comment.component.spec.ts
+++ src/app/read/comment/comment.component.spec.ts
@@ -1,8 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CommentComponent } from './comment.component';
+import { DebugElement } from '@angular/core';
+import { findElement } from 'src/app/find-elements-helper';
+
+const howToFindHeader = findElement('header')
+  .withClass('comment')
+  .please();
 
 describe('CommentComponent', () => {
+
   let component: CommentComponent;
   let fixture: ComponentFixture<CommentComponent>;
 
@@ -19,7 +25,22 @@ describe('CommentComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should exist', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('header', () => {
+
+    let header: DebugElement;
+
+    beforeEach(() => {
+      header = fixture.debugElement.query(howToFindHeader);
+    });
+
+    it('should exist', () => {
+      expect(header).toBeTruthy();
+    });
+
+  });
+
 });

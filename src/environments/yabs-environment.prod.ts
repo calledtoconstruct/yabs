9feb389e49712f2fb066d6a335c74937afcf161a
Joseph Woolley <joe@calledtoconstruct.net>
2021-11-11 15:19:32 -0500

    Updating configuration

--- src/environments/yabs-environment.prod.ts
+++ src/environments/yabs-environment.prod.ts
@@ -3,13 +3,13 @@ import { Environment } from './environment';
 export const environment = <Environment>{
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDVnaenqdTw7adMVPmqaZ0QxTMHmAqyJ7c',
+    apiKey: 'AIzaSyDIsRy6F4ojb9T0bE4Ap4W3cgQVSvbTIdg',
     authDomain: 'yet-another-blog-site.firebaseapp.com',
     projectId: 'yet-another-blog-site',
     storageBucket: 'yet-another-blog-site.appspot.com',
     messagingSenderId: '853276277319',
-    appId: '1:853276277319:web:1814ff0a0f04d9d4dd4f62',
-    measurementId: 'G-D2X6XJTL0C'
+    appId: '1:853276277319:web:f91b4f282c0d8962dd4f62',
+    measurementId: 'G-9YK25K8X10'
   },
   modules: [
     'check',
